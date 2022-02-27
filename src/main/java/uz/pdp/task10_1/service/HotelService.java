26a24fd6e1e2b7af086f1177f92c36ac16e36a3e
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-02-27 10:50:29 +0500

    hotel service full done successfully!

--- src/main/java/uz/pdp/task10_1/service/HotelService.java
+++ src/main/java/uz/pdp/task10_1/service/HotelService.java
@@ -1,7 +1,42 @@
 package uz.pdp.task10_1.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uz.pdp.task10_1.entity.Hotel;
+import uz.pdp.task10_1.payload.ApiResponse;
+import uz.pdp.task10_1.repository.HotelRepository;
+
+import java.util.Optional;
 
 @Service
 public class HotelService {
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    public ApiResponse saveOrDelete(Hotel hotel){
+        if (hotel.getId()!=null){
+            Hotel byId = hotelRepository.getById(hotel.getId());
+            byId.setName(hotel.getName());
+            hotelRepository.save(byId);
+        }
+        else {
+            hotelRepository.save(hotel);
+        }
+        return new ApiResponse(true,hotel.getId()==null ? "Saved" : "Edited");
+    }
+
+    public ApiResponse getHotelByID(Long id) {
+        Optional<Hotel> optional = hotelRepository.findById(id);
+        return optional.map(hotel -> new ApiResponse(true, "ID: " + id, hotel))
+                .orElseGet(() -> new ApiResponse(false, "Not found Hotel"));
+    }
+
+    public ApiResponse getAll() {
+        return new ApiResponse(true,"All hotels",hotelRepository.findAll());
+    }
+
+    public ApiResponse remove(Long id) {
+        hotelRepository.deleteById(id);
+        return new ApiResponse(true,"Deleted");
+    }
 }
