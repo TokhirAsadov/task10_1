d011edfa3f4de5996f4e50162fc21aa2aa81a691
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-02-27 12:17:16 +0500

    room controller full done

--- src/main/java/uz/pdp/task10_1/controller/RoomController.java
+++ src/main/java/uz/pdp/task10_1/controller/RoomController.java
@@ -1,9 +1,10 @@
 package uz.pdp.task10_1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.task10_1.payload.RoomDto;
 import uz.pdp.task10_1.service.RoomService;
 
 @RestController
@@ -13,6 +14,34 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
+    @GetMapping("/getRoomsByHotelId/{hotel_id}")
+    public HttpEntity<?> getRoomsByHotelId(
+            @PathVariable Long hotel_id,
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size,
+            @RequestParam(value = "all",defaultValue = "true") boolean all
+    ){
+        return ResponseEntity.ok(roomService.getAllRoomsByHotelId(hotel_id,page,size,all));
+    }
 
+    @GetMapping("/delete/{id}")
+    public HttpEntity<?> delete(@PathVariable Long id){
+        return ResponseEntity.ok(roomService.remove(id));
+    }
+
+    @PostMapping("/saveOrDelete")
+    public HttpEntity<?> saveOrEdit(@RequestBody RoomDto dto){
+        return ResponseEntity.ok(roomService.saveOrEdit(dto));
+    }
+
+    @GetMapping("/all")
+    public HttpEntity<?> getAllRooms(){
+        return ResponseEntity.ok(roomService.getAllRooms());
+    }
+
+    @GetMapping("/getRoomById/{id}")
+    public HttpEntity<?> getRoomById(@PathVariable Long id){
+        return ResponseEntity.ok(roomService.getRoomById(id));
+    }
 
 }
