83183028ca3ffdf71431460c912983e6b3f85649
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-02-27 10:48:53 +0500

    add some codes

--- src/main/java/uz/pdp/task10_1/controller/HotelController.java
+++ src/main/java/uz/pdp/task10_1/controller/HotelController.java
@@ -1,7 +1,10 @@
 package uz.pdp.task10_1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hotel")
 public class HotelController {
 }
--- src/main/java/uz/pdp/task10_1/controller/RoomController.java
+++ src/main/java/uz/pdp/task10_1/controller/RoomController.java
@@ -1,7 +1,11 @@
 package uz.pdp.task10_1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/room")
 public class RoomController {
+
 }
